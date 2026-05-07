6c9ed9faa67e63d0f60db54f57d4de77149d944a
Birat <biratdey200311@gmail.com>
2026-05-07 21:15:15 +0530

    A simple thread program -- version 3

---
+++ src/Java8/LambdaExpressions/LambdaDemo8.java
@@ -0,0 +1,22 @@
+//a simple thread program -- version 3
+//in this version, we are using anonymous inner class
+
+package Java8.LambdaExpressions;
+
+public class LambdaDemo8
+{
+    public static void main(String[] args) {
+        //create thread object and pass the object of anonymous class
+        Thread t = new Thread(new Runnable()
+        {
+            //implement the run() of the Runnable class
+            public void run()
+            {
+                System.out.println("This is from anonymous inner class");
+            }
+        });
+
+        //run the thread
+        t.start();
+    }
+}
