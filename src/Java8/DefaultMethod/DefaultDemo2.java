642dd451402e58a6b30f6967ed9f0bc4b0028aa6
Birat <biratdey200311@gmail.com>
2026-05-12 21:50:06 +0530

    Two interfaces with same default method name

---
+++ src/Java8/DefaultMethod/DefaultDemo2.java
@@ -0,0 +1,37 @@
+//A program to understand how to refer to a particular default method when the same method is found in two interfaces.
+//two interfaces with same default method name
+
+package Java8.DefaultMethod;
+
+interface One
+{
+    default void message()
+    {
+        System.out.println("Hello from One");
+    }
+}
+
+interface Two {
+    default void message() {
+        System.out.println("Hello from Two");
+    }
+}
+
+class Impclass implements One, Two
+{
+    //override the message() method to resolve the confusion
+    public void message()
+    {
+        Two.super.message(); //this will call Two's message()
+     }
+}
+
+public class DefaultDemo2
+{
+    public static void main(String[] args)
+    {
+        //create implementation class object
+        Impclass ic = new Impclass();
+        ic.message();
+    }
+}
