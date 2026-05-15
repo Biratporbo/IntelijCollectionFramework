7e01a76f8201cb1827799d0ccc8798368b79d02f
Birat <biratdey200311@gmail.com>
2026-05-15 22:36:56 +0530

    Predicate to display required numbers from the list

---
+++ src/CollectionFramework/Predicates/PredicateDemo4.java
@@ -0,0 +1,33 @@
+package CollectionFramework.Predicates;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo4
+{
+    public static void main(String[] args)
+    {
+        //take a group of Integer objects in an array
+        Integer []arr = {8,9,10,11,12,13,14,15};
+
+        //create Predicate references
+        Predicate<Integer>gt,lt;
+
+        //lambda expression to return true if the number > 10
+        gt = (n) -> n > 10;
+
+        //lambda expression to return true if the number < 15
+        lt = (n) -> n < 15;
+
+        System.out.println("Numbers > 10 and < 15:");
+        display(gt.and(lt), arr);
+
+        System.out.println("\nNumbers <= 10 and >= 15: ");
+        display(gt.and(lt).negate(), arr);
+    }
+
+    static void display(Predicate<Integer> p, Integer []arr)
+    {
+        for(Integer i: arr)
+            if(p.test(i)) System.out.println(i+ " ");
+    }
+}
