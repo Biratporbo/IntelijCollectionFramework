c5f75e4e2a47d18a0c064ea6a77115950ae2ddb3
Birat <biratdey200311@gmail.com>
2026-05-16 20:50:23 +0530

    Function to find length of a given string -- changing part

--- src/CollectionFramework/Functions/FunctionDemo1.java
+++ src/CollectionFramework/Functions/FunctionDemo1.java
@@ -8,7 +8,8 @@ public class FunctionDemo1
 {
     public static void main(String[] args)
     {
-        // create a Function reference to lambda expression to find length of a string */
+        /* create a Function reference to lambda expression
+            to find length of a string */
 
         Function<String, Integer> len = (str) -> str.length();
 
