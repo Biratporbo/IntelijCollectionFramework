c7a2f33fe126ae2815137105811efb8837365672
Birat <biratdey200311@gmail.com>
2026-05-21 21:33:03 +0530

    To convert the elements of a stream into an array

---
+++ src/StreamsAPI/Convert2.java
@@ -0,0 +1,33 @@
+//A program to retrieve the numbers from an ArrayList and convert the resultant stream into an array
+
+package StreamsAPI;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class Convert2
+{
+    public static void main(String[] args)
+    {
+        //create a list to store Integer objects
+        List<Integer> lst = new ArrayList<Integer>();
+
+        //add an elements to the list
+        for(int i = 1; i < 10; i++)
+        {
+            lst.add(i);
+        }
+
+        //convert this list into stream using stream()
+        Stream<Integer> sm = lst.stream();
+
+        /*filter the elements which are lesser than 5 and collect
+        them into an Integer type array using toArray(Integer[]::new) */
+        Integer[] arr = sm.filter(i -> i < 5).toArray(Integer[]::new);
+
+        //display the array
+        for(Integer i: arr)
+            System.out.println(i);
+    }
+}
