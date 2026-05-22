a6c03de6f135d5761f2fbb64b6bb892556985e5c
Birat <biratdey200311@gmail.com>
2026-05-22 20:00:06 +0530

    To create stream of objects using Stream.of()

---
+++ src/StreamsAPI/Create1.java
@@ -0,0 +1,22 @@
+package StreamsAPI;
+
+import java.util.stream.Stream;
+
+public class Create1
+{
+    public static void main(String[] args)
+    {
+        //create a stream of Integer objects using Stream.of() method
+        Stream <Integer> sm1 = Stream.of(10,11,12,13,14,15,16);
+
+        //display the elements of the stream
+        sm1.forEach(System.out::println);
+
+        //create another stream of array of Float objects
+        Float arr[] = {1.1f, 2.2f, 3.3f, 4.4f};
+        Stream<Float> sm2 = Stream.of(arr);
+
+        //display the elements of the stream
+        sm2.forEach(System.out::println);
+    }
+}
