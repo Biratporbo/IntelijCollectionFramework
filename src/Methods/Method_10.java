14cd91dad4e57580cf9d2e1fb142a423d5d7d79c
Birat <biratdey200311@gmail.com>
2026-08-14 22:27:53 +0530

    Add Method_10.java to demonstrate functionality without a main() method

---
+++ src/Methods/Method_10.java
@@ -0,0 +1,11 @@
+// A program without main() method. This program compiles and also runs.
+//No main() method
+
+class Method_10
+{
+    static
+    {
+        System.out.println("Static Block");
+        System.exit(0);
+    }
+}
