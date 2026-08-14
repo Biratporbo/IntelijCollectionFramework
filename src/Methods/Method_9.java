36d1bfe9bd96f68b4811b6363530ad5ffd901de2
Birat <biratdey200311@gmail.com>
2026-08-14 22:27:29 +0530

    Add Method_9.java to demonstrate static block and static method functionality

---
+++ src/Methods/Method_9.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class Method_9 
+{
+    static 
+    {
+        System.out.println("Satic block");
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println("Static method");
+    }
+}
