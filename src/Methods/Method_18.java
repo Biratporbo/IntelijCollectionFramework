9d7bb25bfe5e7848a56055593078b3a5bbe429e1
Birat <biratdey200311@gmail.com>
2026-08-19 23:16:48 +0530

    Add Method_18.java to generate a specified number of prime numbers using methods

---
+++ src/Methods/Method_18.java
@@ -0,0 +1,50 @@
+// A program to generate requried number of primes using methods
+
+package Methods;
+
+//Prime number series
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Prime
+{
+    //to test and return true if num is prime
+    static boolean prime(long num)
+    {
+        //initially isPrime is true, it becomes false if
+        //num is not prime
+        boolean isPrime = true;
+        //from 2 to num-1, if any number divides num, it is not prime
+        for(int i=2; i<=num-1; i++)
+            if(num % i==0) isPrime = false;
+        return isPrime;
+    }
+    //accept how many primes required into max
+    // c is counter for no. of primes generated
+    static void generate(long max)
+    {
+        long c=1, num=2;
+        while(c<=max)
+        {
+            if(prime(num))  //call prime() method directly
+            {
+                System.out.println(num);
+                ++c;
+            }
+            ++num;
+        }
+    }
+}
+public class Method_18 {
+    public static void main(String[] args) throws IOException
+    {
+        //accept the number of primes are needed
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How many primes? ");
+        int max = Integer.parseInt(br.readLine());
+        //generate max number of primes
+        Prime.generate(max);
+    }
+}
